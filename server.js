4640638c91ebe3b88bf2398b9a9d45a2f8d10d2c
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-05-19 13:08:23 +0900

    portfolio publishid to github

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
-console.log("web serverni boshlash");
 const express = require("express");
-const http = require("http");
 const app = express();
+const http = require("http");
 const fs = require("fs");
+const path = require("path")
 
-//1.kirish kodlari
+let user;
+ 
+function loadUserData(callback) {
+  fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading user data:", err);
+      callback(err, null); // Pass the error to the callback
+    } else {
+      user = JSON.parse(data);
+      callback(null, user); // Pass the user data to the callback
+    }
+  });
+}
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//3.views code
-app.set("views", "public");
+
+app.set("views", "views");
 app.set("view engine", "ejs");
-//server xosil qilamiz
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT,  () => {
-  console.log(`The server is running successfully on port:${PORT}`);
+app.set('views', path.join(__dirname, 'views'));
+
+app.get("/author", (req, res) => {
+  // Load user data before rendering
+  loadUserData((err, userData) => {
+    if (err) {
+      // Handle the error (e.g., render an error page)
+      res.status(500).send('Error loading user data');
+    } else {
+      res.render("author", { user: userData });
+    }
+  });
+});
+
+const port = 3006;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
