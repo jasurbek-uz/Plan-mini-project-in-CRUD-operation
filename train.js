6c32b9670e50312eb3ddf9980d94b5d028fd726e
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-05-22 19:08:16 +0900

    Feat: uyga vazifa

--- train.js
+++ train.js
@@ -25,35 +25,35 @@ console.log(countLetter("e", "engineer"));
 
 // event loop va callback
 
-console.log("Jack ma maslahatlari");
-const list =[
-    "yaxshi talaba noling", //0-20
-    "togri bsohliq tanglang va koproq xato qiling", //20-30
-    "ozingizga ishlashni boshlang", // 30-40
-    "sizni   kuchli qiladigan narsalarni qiling", //40-50
-    "yoshlarga investitsiya qiling" //50-60
-    "endi dam oling, foydasi yoq" //60
-]; 
-function maslahatBering (a, callback) {
-    if (typeof a !== "number") callback ("insert a number", null)
-    else if (a<=20) callback (null, list[0]);
-    else if (a >= 20 && a <= 30) callback(null,list[1]);
-    else if (a >= 30 && a <= 40) callback(null, list[2]);
-    else if (a >= 40 && a <= 50) callback(null, list[3]);
-    else if (a >= 50 && a <= 60) callback(null, list[4]);
-    else {
-        // callback(null, list[5]);
-        setTimeout(function(){
-            callback list[5];
-        }, 5000);
-    }
-}
+// console.log("Jack ma maslahatlari");
+// const list =[
+//     "yaxshi talaba noling", //0-20
+//     "togri bsohliq tanglang va koproq xato qiling", //20-30
+//     "ozingizga ishlashni boshlang", // 30-40
+//     "sizni   kuchli qiladigan narsalarni qiling", //40-50
+//     "yoshlarga investitsiya qiling", //50-60
+//     "endi dam oling foydasi yoq" //60
+// ]; 
+// function maslahatBering (a, callback) {
+//     if (typeof a !== "number") callback ("insert a number", null)
+//     else if (a<=20) callback (null, list[0]);
+//     else if (a >= 20 && a <= 30) callback(null,list[1]);
+//     else if (a >= 30 && a <= 40) callback(null, list[2]);
+//     else if (a >= 40 && a <= 50) callback(null, list[3]);
+//     else if (a >= 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         // callback(null, list[5]);
+//         setTimeout(function(){
+//             return list[5];
+//         }, 5000);
+//     }
+// }
 
-console.log("passed here 0");
-maslahatBering(25,(err, data) =>{
-    if (err) console.log("ERROR:", err);
-    console.log("javob:", data);
-});
+// console.log("passed here 0");
+// maslahatBering(25,(err, data) =>{
+//     if (err) console.log("ERROR:", err);
+//     console.log("javob:", data);
+// });
 
 // async function  
 
@@ -78,11 +78,11 @@ maslahatBering(25,(err, data) =>{
     //     }, 1000);
     // });
 
-    console.log("passed here 0");
-maslahatBering(25,(err, data) =>{
-    if (err) console.log("ERROR:", err);
-    console.log("javob:", data);
-});
+//     console.log("passed here 0");
+// maslahatBering(25,(err, data) =>{
+//     if (err) console.log("ERROR:", err);
+//     console.log("javob:", data);
+// });
 // // then va catch
 // console.log("passed here 0");
 // maslahatBering(25)
