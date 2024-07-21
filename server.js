c7993e6ba869c6590e28b819d8c286c85555ccc8
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-07-21 10:07:45 +0900

    fix: some text

--- server.js
+++ server.js
@@ -44,33 +44,3 @@ mongodb.connect(connectionString,
 
 
 
-
-// const http = require("http");
-// const mongodb = require ("mongodb");
-
-// // mongodb ga ulanish 
-// let db;
-//  const connectionString = "mongodb+srv://braveredmi:PFGsplNheMyPFHhn@cluster0.3mre5hw.mongodb.net/plan"
-
-
-//  mongodb.connect(
-//   connectionString,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   (err, client) => {
-//     if (err) console.log("Error on connection MongoDB: ", err.message);
-//     else {
-//       console.log("MongoDB connected successfully!");
-//       module.exports = client;
-//       const app = require("./app");
-//       const server = http.createServer(app);
-
-//       const PORT = 3000;
-//       server.listen(PORT, (err, res) => {
-//         console.log(`Server is running on port: ${PORT}`);
-//       });
-//     }
-//   }
-// );
